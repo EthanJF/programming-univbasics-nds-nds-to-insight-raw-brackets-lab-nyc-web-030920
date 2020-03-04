39023c4a5342431a53c4e4c81cebcbaac386929f
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-04 19:48:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,16 +18,16 @@ def directors_totals(nds)
   # ...
   #
   #
-  director_index = 0
-  while director_index < nds.length do
-    result[nds[director_index][:name]] = 0
-    movie_index = 0
-    while movie_index < nds[director_index].length
-    result[nds[director_index][:name]] += nds[director_index][movie_index][:worldwide_gross]
-    movie_index += 1
-  end
-    director_index += 1
-  end
+  # director_index = 0
+  # while director_index < nds.length do
+  #   result[nds[director_index][:name]] = 0
+  #   movie_index = 0
+  #   while movie_index < nds[director_index].length
+  #   result[nds[director_index][:name]] += nds[director_index][movie_index][:worldwide_gross]
+  #   movie_index += 1
+  # end
+  #   director_index += 1
+  # end
   # Be sure to return the result at the end!
   puts pp nds
 end
